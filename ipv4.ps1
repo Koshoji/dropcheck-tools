90127483ec69033a2351a590e2680984461a34c9
koshoji <koshoji@eaglejump.jp>
2021-04-08 11:08:32 +0900

    fix ipv4 addrつけるやつ

--- ipv4.ps1
+++ ipv4.ps1
@@ -14,7 +14,9 @@ $Gateway = $args[4]
 if ($InterfaceDhcp -eq "true") {
     Set-NetIPInterface -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -Dhcp Enabled
 } elseif ($InterfaceDhcp -eq "false") {
-    Set-NetIPInterface -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -Dhcp Disabled
-    Set-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress $Ipv4Address -PrefixLength $Ipv4Prefix -DefaultGateway $Gateway
-    Set-NetIPInterface -InterfaceIndex $InterfaceIndex -ServerAddresses 1.1.1.1
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\services\Tcpip\Parameters\Interfaces\$((Get-NetAdapter -InterfaceIndex $InterfaceIndex).InterfaceGuid)" -Name EnableDHCP -Value 0
+    Remove-NetIpAddress -InterfaceIndex $InterfaceIndex -AddressFamily IPv4
+    Remove-NetRoute -InterfaceIndex $InterfaceIndex -AddressFamily IPv4 -Confirm:$false
+    New-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress $Ipv4Address -PrefixLength $Ipv4Prefix -DefaultGateway $Gateway
+    Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddress 1.1.1.1
 }
